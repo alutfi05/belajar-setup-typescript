2174a79528d82a98d181095f3dd61c9f26463c57
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-11 14:56:00 +0700

    learning how to use access modifier public and private in typescript

--- src/classes.ts
+++ src/classes.ts
@@ -1,7 +1,8 @@
 class User {
-    email: string;
-    name: string;
-    readonly city: string = 'Depok';
+    public email: string;
+    name: string; // default is public
+    private city: string = 'Depok'; // cannot access and set
+    // readonly city: string = 'Depok'; // can access but cannot set
 
     constructor(email: string, name: string) {
         this.email = email;
@@ -9,5 +10,19 @@ class User {
     }
 }
 
+// another option
+class User2 {
+    readonly city: string = 'Depok';
+
+    constructor(
+        public email: string,
+        public name: string,
+        private userId: string
+    ) {}
+}
+
 const lutfi = new User('lutfi@gmail.com', 'lutfi');
 // lutfi.city = 'Jakarta'; // error
+// lutfi.city; // error
+
+const laila = new User2('laila@gmail.com', 'laila', 'userXYZ');

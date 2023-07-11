34656fbd22d7a3bed3dbf4c8d248f2e56449b4fe
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-11 15:09:58 +0700

    learning getter and setter in typescript

--- src/classes.ts
+++ src/classes.ts
@@ -12,6 +12,7 @@ class User {
 
 // another option
 class User2 {
+    private _courseCount = 1;
     readonly city: string = 'Depok';
 
     constructor(
@@ -19,6 +20,28 @@ class User2 {
         public name: string,
         private userId: string
     ) {}
+
+    private deleteToken() {
+        console.log('Token Deleted');
+    }
+
+    // Getter
+    get getAppleEmail(): string {
+        return `Apple ${this.email}`;
+    }
+
+    get getCourseCount(): number {
+        return this._courseCount;
+    }
+
+    // Setter
+    set courseCount(courseNumber: number) {
+        if (courseNumber <= 1) {
+            throw new Error('Course count should be more than 1');
+        }
+
+        this._courseCount += courseNumber;
+    }
 }
 
 const lutfi = new User('lutfi@gmail.com', 'lutfi');

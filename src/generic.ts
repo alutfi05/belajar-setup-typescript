d73204b5a1148cd64bfec8d3bf3f8f569c5708f6
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-12 11:24:23 +0700

    learning generics in typescript

---
+++ src/generic.ts
@@ -0,0 +1,29 @@
+const score: Array<number> = [];
+const names: Array<string> = [];
+
+function identityOne(val: boolean | number): boolean | number {
+    return val;
+}
+
+function identityTwo(val: any): any {
+    return val;
+}
+
+function identityThree<Type>(val: Type): Type {
+    return val;
+}
+
+identityThree(3);
+identityThree('lutfi');
+identityThree(true);
+
+function identityFour<T>(val: T): T {
+    return val;
+}
+
+interface Bottle {
+    brand: string;
+    type: number;
+}
+
+console.log(identityFour<Bottle>({ brand: 'Coca - Cola', type: 3 }));
